53c2a119a42baec451b09dc03f985c33805aa73e
bharath-alle <bharath-alle@users.noreply.github.com>
2019-06-17 22:28:32 +0530

    Add files via upload

---
+++ SampleWepApi/SampleWepApi/Controllers/Employee.cs
@@ -0,0 +1,9 @@
+﻿namespace SampleWepApi.Controllers
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ SampleWepApi/SampleWepApi/Controllers/EmployeeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SampleWepApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult<IEnumerable<string>> CreateEmployee(Employee employee)
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
